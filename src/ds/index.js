5887de5682a160e10320092211af1ecf1981764e
Arlen Johnson <arlen@dogenmedia.com>
2019-12-31 12:24:13 -0500

    NIAID customizations to DS. This includes numerous changes to index.html (primarily to header, footer, and the list of common choices) and one change to the index.js file to include our CSS customizations. All other additions are found in src/assets/niaid.

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,4 +1,5 @@
 import '../assets/ds.scss';
+import '../assets/niaid/niaid.scss';
 
 import { dom, library, config } from '@fortawesome/fontawesome-svg-core';
 import {faPlusSquare} from '@fortawesome/free-solid-svg-icons/faPlusSquare';
